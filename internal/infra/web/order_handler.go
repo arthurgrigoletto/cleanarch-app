fe9c89cc450c7646dab78e94c6d582d1532fd781
Arthur Grigoletto <arthur.grigoletto.lima@gmail.com>
2023-07-17 16:30:19 -0300

    finish clean architeture module

---
+++ internal/infra/web/order_handler.go
@@ -0,0 +1,50 @@
+package web
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/arthurgrigoletto/cleanarch-app/internal/entity"
+	"github.com/arthurgrigoletto/cleanarch-app/internal/usecase"
+	"github.com/arthurgrigoletto/cleanarch-app/pkg/events"
+)
+
+type WebOrderHandler struct {
+	EventDispatcher   events.EventDispatcherInterface
+	OrderRepository   entity.OrderRepositoryInterface
+	OrderCreatedEvent events.EventInterface
+}
+
+func NewWebOrderHandler(
+	eventDispatcher events.EventDispatcherInterface,
+	orderRepository entity.OrderRepositoryInterface,
+	orderCreatedEvent events.EventInterface,
+) *WebOrderHandler {
+	return &WebOrderHandler{
+		EventDispatcher:   eventDispatcher,
+		OrderRepository:   orderRepository,
+		OrderCreatedEvent: orderCreatedEvent,
+	}
+}
+
+func (h *WebOrderHandler) Create(w http.ResponseWriter, r *http.Request) {
+	var dto usecase.OrderInputDTO
+	err := json.NewDecoder(r.Body).Decode(&dto)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	createOrder := usecase.NewCreateOrderUseCase(h.OrderRepository, h.OrderCreatedEvent, h.EventDispatcher)
+	output, err := createOrder.Execute(dto)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = json.NewEncoder(w).Encode(output)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
